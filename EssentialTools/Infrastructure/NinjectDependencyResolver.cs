c5f3121ea52e561665e3f262ddf0b6bc85618e7c
Nick Home <icthos@gmail.com>
2016-04-16 13:28:57 -0500

    Created the Dependency Resolver

---
+++ EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using EssentialTools.Models;
+using Ninject;
+
+namespace EssentialTools.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernal;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernal = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernal.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernal.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            kernal.Bind<IValueCalculator>().To<LinqValueCalculator>();
+        }
+    }
+}
