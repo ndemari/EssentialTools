03cae1c57dfc2cf28792c906246890dc8faecaf5
Nick Home <icthos@gmail.com>
2016-04-16 14:50:11 -0500

    Using conditional bindings

--- EssentialTools/Infrastructure/NinjectDependencyResolver.cs
+++ EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -30,6 +30,7 @@ namespace EssentialTools.Infrastructure
         {
             kernal.Bind<IValueCalculator>().To<LinqValueCalculator>();
             kernal.Bind<IDiscountHelper>().To<DefaultDiscountHelper>().WithConstructorArgument("discountParam", 50M);
+            kernal.Bind<IDiscountHelper>().To<FlexibleDiscountHelper>().WhenInjectedInto<LinqValueCalculator>();
         }
     }
 }
